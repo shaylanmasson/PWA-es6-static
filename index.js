09c64cc1afc4a7646a29a6e9b98901e41c38ba2e
shaylanmasson <shaylan.masson@gmail.com>
2021-04-19 14:51:19 -0400

    changed from heroku to localhost payment

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const Page = require("./_layout/Default");
 
 module.exports = class extends Page {
     constructor(){
-        super({title:"Home", sName:"Richard Hildred"});
+        super({title:"Home", sName:"Shaylan Masson"});
     }
     render(sPage) {
-        const oJson = fetch("https://prog8110winter2021section2-default-rtdb.firebaseio.com/meals.json").json();
+        const oJson = fetch("https://ux308finalexam-default-rtdb.firebaseio.com/meals.json").json();
         console.log(oJson);
-        let sResult = "<h1>Upcoming Popup Meals</h1>";
+        let sResult = "<h1>Upcoming Stay-at-Home Craft Kits</h1>";
         Object.keys(oJson).map((key) => {
             const oEntity = oJson[key];
             console.log(oEntity);
@@ -17,10 +17,13 @@ module.exports = class extends Page {
             <h2>${oEntity.title}</h2>
             <p><img src="${oEntity.featured_image}" alt="${oEntity.title}"</p>
             <p>${oEntity.full_description}</p>
-            <form action="https://serene-taiga-04277.herokuapp.com/payment" method="post">
+            <p>${oEntity.kit_date}</p>
+            <p>${oEntity.kit_location}</p>
+            <p>${oEntity.kit_price}</p>
+            <form action="https://localhost:3001/payment" method="post">
             <input type="hidden" name="title" value="${oEntity.title}" />
-            <input type="hidden" name="price" value="21" />
-            <input type="tel" placeholder="enter your number" name="telephone"/>
+            <input type="hidden" name="price" value="${oEntity.price}" />
+            <input type="tel" placeholder="enter your number"/>
             <button type="submit">Order now</button>
             </form>
             `;
